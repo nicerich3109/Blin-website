c21b90ad066fc2081a7c13e4d2a74172c7852e47
TimonkaBlin <117970456+nicerich3109@users.noreply.github.com>
2026-09-03 09:10:54 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Public frontend configuration. This file must contain only public values.
 // Set this to the HTTPS address where the Blin Bot Dashboard API is hosted.
-window.BLIN_API_BASE = 'https://bot-1788413556-1157-darkknighttt.bothost.tech/webhook';
+window.BLIN_API_BASE = 'https://bot-1788413556-1157-darkknighttt.bothost.tech/';
